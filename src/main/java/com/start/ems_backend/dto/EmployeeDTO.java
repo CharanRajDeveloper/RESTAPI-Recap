5dc5e5d8adf296a4f7bb0e8be6ae92290467b75d
charanraj <charanrajjb007@gmail.com>
2026-06-06 00:48:39 +0530

    changes

--- src/main/java/com/start/ems_backend/dto/EmployeeDTO.java
+++ src/main/java/com/start/ems_backend/dto/EmployeeDTO.java
@@ -2,6 +2,7 @@ package com.start.ems_backend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -12,9 +13,11 @@ public class EmployeeDTO {
     private Long id;
 
     @NotBlank(message = "First name is required")
+    @Size(max = 50,message = "First name can not exceed 50 characters")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name cannot exceed 50 characters")
     private String lastName;
 
     @NotBlank(message = "Email is required")
--- src/main/java/com/start/ems_backend/entity/Employee.java
+++ src/main/java/com/start/ems_backend/entity/Employee.java
@@ -16,16 +16,12 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "First name is required")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotBlank(message = "Last name is required")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
 }
